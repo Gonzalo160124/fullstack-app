0a629b5f1d73747554479f6a82af99034f64780f
Gonzalo160124 <gonmopi00@gmail.com>
2026-05-10 18:20:18 +0200

    chore: variable de entorno para la URL de la API

--- src/api/animeApi.ts
+++ src/api/animeApi.ts
@@ -1,6 +1,6 @@
 import type { AnimeAPI } from "../types/tipos";
 
-const BASE_URL = "http://localhost:3001/api/v1";
+const BASE_URL = import.meta.env.VITE_API_URL ?? "http://localhost:3001/api/v1";
 
 
 async function peticion<T>(url: string): Promise<T> {
